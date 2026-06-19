51f8a19bb986f398c101d687e082d91a617c40c0
Alexander Spiegel <spiegel.alexander@gmail.com>
2026-06-19 18:33:47 +0200

    small optimization

--- src/BigFileProcessing/BusinessLogic/Services/Implementations/RowDataParser.cs
+++ src/BigFileProcessing/BusinessLogic/Services/Implementations/RowDataParser.cs
@@ -30,7 +30,7 @@ namespace BusinessLogic.Services.Implementations
         #region Public Methods
         public BLO.RowData? Parse(string line)
         {
-            if (string.IsNullOrWhiteSpace(line) == true)
+            if (string.IsNullOrEmpty(line) == true)
             {
                 _logger.LogError("input line is null or whitespace.");
                 return null;
@@ -50,15 +50,20 @@ namespace BusinessLogic.Services.Implementations
                 return null;
             }
 
-            string text = line[(dotIndex + 1)..];
+            // the explanation below
 
-            if (string.IsNullOrWhiteSpace(text) == true)
+            string text = line[(dotIndex + 2)..];
+
+            // done for optinization, to avoid creating a new string allocation for the trimmed text.
+            // we can do it (dotIndex + 2) because incoming format is guaranteed to be "number. text" (with a space after the dot).
+
+            if (string.IsNullOrEmpty(text) == true)
             {
                 _logger.LogError("empty text part. input line: '{Line}'", line);
                 return null;
             }
 
-            return new BLO.RowData(Number: number, Text: text.Trim());
+            return new BLO.RowData(Number: number, Text: text);
         }
 
         #endregion
